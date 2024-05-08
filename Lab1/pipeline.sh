937b9f0ac12d2f244daafbbffaacf88a78213ea0
Sibbear1980 <ds.shabanov@yandex.ru>
2024-05-09 00:16:04 +0300

    Initial commit

---
+++ Lab1/pipeline.sh
@@ -0,0 +1,9 @@
+echo "MLops Labs 1. Shabanov DS"
+echo "Step 1. Run python script - data_creation.py"
+python3 data_creation.py
+echo "Step 2. Run python script - model_preparation.py"
+python3 model_preparation.py
+echo "Step 3. Run python script - model_preprocessing.py"
+python3 model_preprocessing.py
+echo "Step 4. Run python script - model_testing.py"
+python3 model_testing.py
